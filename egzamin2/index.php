ea0537275e5084865b6cbeafd584ce5edfa9c4dd
Adamek303 <boy1234ksg@gmail.com>
2026-04-26 10:02:03 +0200

    egzamin 2025 czerwiec zad 10 php, zrobiony html i 2 skrypty

---
+++ egzamin2/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Szkolenia i kursy</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <header>
+        <h1>SZKOLENIA</h1>
+    </header>
+
+    <main>
+        <section id="lewy">
+            <!-- <table>
+                <tr> <th>Kurs</th>
+                    <th>Nazwa</th>
+                    <th>Cena</th>
+                </tr>
+                </table> -->
+            <!--skrypt1-->
+            <?php
+            $db = mysqli_connect('localhost', 'root', '', 'szkolenia');
+            $q = '  SELECT kursy.kod, kursy.nazwa, kursy.cena 
+                        FROM kursy
+                        ORDER BY kursy.cena asc';
+            $mq = mysqli_query($db, $q);
+            echo '<table>';
+            echo '<tr>';
+            echo '<th>Kurs</th>';
+            echo '<th>Nazwa</th>';
+            echo '<th>Cena</th>';
+            echo '</tr>';
+            while ($row = mysqli_fetch_assoc($mq)) {
+                echo '<tr>';
+                echo '<td><img src="' . $row['kod'] . '.jpg" alt="kurs"></td>';
+                echo '<td>' . $row['nazwa'] . '</td>';
+                echo '<td>' . $row['cena'] . '</td>';
+                echo '</tr>';
+
+            }
+            echo '</table>';
+            ?>
+        </section>
+
+        <section id="right">
+            <h2>Zapisy na kursy</h2>
+            <form action="" method="POST"> <label for="imie">Imie</label>
+                <input type="text" name="imie" id="imie"><br> <label for="nazwisko">Nazwisko</label>
+                <input type="text" name="nazwisko" id="nazwisko"><br>
+
+                <label for="wiek">Wiek</label>
+                <input type="number" name="wiek" id="wiek"><br>
+
+                <label for="opcje">Rodzaj kursu</label>
+                <select id="opcje">
+                    <!---skrypt2-->
+                    <?php
+                    $q = 'SELECT kursy.nazwa
+FROM kursy';
+                    $mq = mysqli_query($db, $q);
+                    while($row = mysqli_fetch_assoc($mq)){
+                        echo '<option>'. $row['nazwa'] . '</option>';
+
+                    }
+
+                    ?>
+                </select><br>
+
+                <input type="button" value="Dodane dane" name="wyslij" id="wyslij">
+                <!--skrypt3-->
+            </form>
+        </section>
+    </main>
+
+    <footer>
+        <p>Strone wykonał: 00000000</p>
+    </footer>
+</body>
+
+</html>
